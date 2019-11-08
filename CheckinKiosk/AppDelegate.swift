df1e5b4b05a874cebfc4749c739e1a84f011e038
Nicholas <nka6bv@virginia.edu>
2019-11-08 14:02:11 -0500

    Initial Commit

---
+++ CheckinKiosk/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CheckinKiosk
+//
+//  Created by Nick Anselmo on 11/8/19.
+//  Copyright © 2019 Nick Anselmo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CheckinKiosk/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CheckinKiosk
+//
+//  Created by Nick Anselmo on 11/8/19.
+//  Copyright © 2019 Nick Anselmo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
